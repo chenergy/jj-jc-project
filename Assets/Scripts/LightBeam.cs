73d4c32e19fa8eed8df3ed2a61fbb7578b84c816
James Lee <jamesjlee@outlook.com>
2018-01-27 23:49:56 -0800

    Additional fixes to levels 2 and 4

--- Assets/Scripts/LightBeam.cs
+++ Assets/Scripts/LightBeam.cs
@@ -27,11 +27,12 @@ public class LightBeam : MonoBehaviour
 	// Update is called once per frame
 	void FixedUpdate ()
 	{
-		this.transform.position += this.direction * SPEED * Time.fixedDeltaTime;
-		this.phase = (this.phase + Time.fixedDeltaTime) % (PERIOD);
 		this.GetComponent<TrailRenderer>().startColor = new Color(.5f + Mathf.Sin(2 * Mathf.PI * phase / PERIOD)/2f , .5f + Mathf.Cos(2 * Mathf.PI * phase / PERIOD)/2f, 0);
 		this.GetComponent<TrailRenderer>().endColor = new Color(.5f + Mathf.Sin(2 * Mathf.PI * phase / PERIOD)/2f, .5f +  Mathf.Cos(2 * Mathf.PI * phase / PERIOD)/2f , 0);
-        if(this.transform.position.sqrMagnitude >= 10000)
+        this.transform.position += this.direction * SPEED * Time.fixedDeltaTime;
+        this.phase = (this.phase + Time.fixedDeltaTime) % (PERIOD);
+
+        if (this.transform.position.sqrMagnitude >= 10000)
         {
             Destroy(this.gameObject);
         }
